5e2a5cae93e761f22eb54a658884749374a8f33b
vinirf <vinicius_resendewii@hotmail.com>
2014-11-28 16:16:19 -0200

    Merge

---
+++ Composicao-Edicao/Componentes Edicao/DataBasePartituraSalva.m
@@ -0,0 +1,41 @@
+//
+//  DataBasePartituraSalva.m
+//  FazendaDoReMi
+//
+//  Created by VINICIUS RESENDE FIALHO on 28/11/14.
+//  Copyright (c) 2014 EMERSON BARROS | VINICIUS RESENDE. All rights reserved.
+//
+
+#import "DataBasePartituraSalva.h"
+
+@implementation DataBasePartituraSalva
+
+//Singleton
++(DataBasePartituraSalva*)sharedManager{
+    static DataBasePartituraSalva *mascote = nil;
+    if(!mascote){
+        mascote = [[super allocWithZone:nil] init];
+    }
+    return mascote;
+}
+
+-(id)init{
+    self = [super init];
+    if(self){
+        self.listaPartiturasSalvas = [[NSMutableArray alloc]init];
+    }
+    return self;
+}
+
++(id)allocWithZone:(struct _NSZone *)zone{
+    return [self sharedManager];
+}
+
+
+-(void)salvarPartitura{
+    Partitura *partitura = [[Partitura alloc]init];
+    
+    
+}
+
+@end
---
+++ Composicao-Edicao/Componentes Edicao/SalvarPartituraViewController.m
@@ -0,0 +1,47 @@
+//
+//  SalvarPartituraViewController.m
+//  FazendaDoReMi
+//
+//  Created by VINICIUS RESENDE FIALHO on 28/11/14.
+//  Copyright (c) 2014 EMERSON BARROS | VINICIUS RESENDE. All rights reserved.
+//
+
+#import "SalvarPartituraViewController.h"
+
+@interface SalvarPartituraViewController ()
+
+@end
+
+@implementation SalvarPartituraViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)btnSalvar:(id)sender {
+    
+    
+    
+}
+@end
