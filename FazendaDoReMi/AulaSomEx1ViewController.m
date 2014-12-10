6eb3949f8987827956711db14fca9a64ca7e8c38
Emerson Barros <emerson.sbarros@gmail.com>
2014-12-09 23:36:45 -0200

    Refactoring aula

--- FazendaDoReMi/AulaSomEx1ViewController.m
+++ FazendaDoReMi/AulaSomEx1ViewController.m
@@ -22,10 +22,10 @@
 //Adiciona os Itens na tela
 //Animados
     [[ControladorDeItem sharedManager]retornaItem :@"tambor"    :self.imgTambor:    @"gestureTap:1:1 + 1 + 2:0:1 + 7:tamborTocando:5:YES:YES:0"];
-//    [[ControladorDeItem sharedManager]retornaItem :@"xilofone"  :self.imgXilofone:  @"gestureTap:1 + 1 + 2:0:1 + 7:xilofoneTocando:5:YES:YES:0"];
+    [[ControladorDeItem sharedManager]retornaItem :@"xilofone"  :self.imgXilofone:  @"gestureTap:1:1 + 1 + 2:0:1 + 7:xilofoneTocando:5:YES:YES:0"];
     [[ControladorDeItem sharedManager]retornaItem :@"saxofone"  :self.imgSaxfone:   @"gestureTap:1:1 + 1 + 2:0:1 + 7:saxofoneTocando:5:YES:YES:0"];
     
-//    [[ControladorDeItem sharedManager]retornaItem :@"violao"    :self.imgViolao:    @"gestureTap:3 + 1 + 2:0:1 + 3:5.0:5.0:1.0:1.5"];
+    [[ControladorDeItem sharedManager]retornaItem :@"violao"    :self.imgViolao:    @"gestureTap:1:1 + 1 + 2:0:1 + 3:5.0:5.0:1.0:1.5"];
     [[ControladorDeItem sharedManager]retornaItem :@"piano"     :self.imgPiano:     @"gestureTap:1:1 + 1 + 2:0:1"];
     [[ControladorDeItem sharedManager]retornaItem :@"flauta"    :self.imgFlauta:    @"gestureTap:1:1 + 1 + 3:5.0:5.0:1.0:1.5 + 2:0:1"];
     [[ControladorDeItem sharedManager]retornaItem :@"chocalho"  :self.imgChocalho:  @"gestureTap:1:1 + 1 + 3:5.0:5.0:1.0:1.5"];
@@ -37,7 +37,16 @@
     [[ControladorDeItem sharedManager]retornaItem:@"quadroZecao":self.imgQuadro         :@"gestureTap:1:1 + 5:1:1:NO:0:400"];
     
 //Itens requeridos para a próxima aula
-    [[ControladorDeItem sharedManager]chamaVerificador:[NSArray arrayWithObjects:self.imgFlauta,self.imgPiano,self.imgTambor,self.imgSaxfone,self.imgChocalho,self.imgViolino,nil]];
+    [[ControladorDeItem sharedManager]chamaVerificador:[NSArray arrayWithObjects:
+                                                        self.imgXilofone,
+                                                        self.imgViolao,
+                                                        self.imgFlauta,
+                                                        self.imgPiano,
+                                                        self.imgTambor,
+                                                        self.imgSaxfone,
+                                                        self.imgChocalho,
+                                                        self.imgViolino,
+                                                        nil]];
 }
 
 
